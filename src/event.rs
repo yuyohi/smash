f1039c80f4d9790f5dbe139ce752ff45e37b7c25
yuyohi <41873114+yuyohi@users.noreply.github.com>
2022-10-27 15:47:59 +0900

    feat: render prompt

--- src/event.rs
+++ src/event.rs
@@ -8,7 +8,7 @@ use tracing::debug;
 
 #[derive(Clone, Debug)]
 struct UserInput {
-    input: String
+    input: String,
 }
 
 #[derive(Clone, Debug)]
@@ -16,7 +16,7 @@ pub struct SmashState {
     columns: usize,
     lines: usize,
     prompt_len: usize,
-    input: UserInput
+    input: UserInput,
 }
 
 impl UserInput {
@@ -38,10 +38,62 @@ impl SmashState {
     }
 
     pub fn render_prompt(&mut self) {
-        let screen_size = terminal::size().unwrap()
+        let screen_size = terminal::size().unwrap();
+        self.columns = screen_size.0 as usize;
+        self.lines = screen_size.1 as usize;
+
+        debug!(?self.columns);
+
+        let mut stdout = std::io::stdout();
+        queue!(
+            stdout,
+            SetAttribute(Attribute::Bold),
+            SetAttribute(Attribute::Reverse),
+            Print("$"),
+            SetAttribute(Attribute::Reset),
+            Print(&format!(
+                "{space:>width$}\r",
+                space = " ",
+                width = self.columns - 1
+            ))
+        )
+        .ok();
+
+        let (mut prompt_str, mut prompt_len) = (String::new(), 0);
+        prompt_str.push_str(" $ ");
+        queue!(stdout, Print(prompt_str.replace('\n', "\r\n"))).ok();
+        prompt_len += prompt_str.len();
+        stdout.flush().unwrap();
+
+        self.prompt_len = prompt_len;
     }
 
     pub fn run(&mut self) {
-        todo!()
+        enable_raw_mode().ok();
+        self.render_prompt();
+
+        debug!("start");
+        'main: loop {
+            match crossterm::event::poll(Duration::from_millis(100)) {
+                Ok(true) => loop {
+                    if let Ok(TermEvent::Key(ev)) = crossterm::event::read() {
+                        match (ev.code, ev.modifiers) {
+                            (KeyCode::Char(ch), KeyModifiers::NONE) => {
+                                self.input.input.push(ch);
+                                debug!(?self.input.input);
+                            }
+                            (KeyCode::Esc, KeyModifiers::NONE) => break 'main,
+                            _ => (),
+                        }
+                    }
+
+                    match crossterm::event::poll(Duration::from_millis(0)) {
+                        Ok(true) => (),
+                        _ => break,
+                    }
+                },
+                _ => (),
+            }
+        }
     }
 }
